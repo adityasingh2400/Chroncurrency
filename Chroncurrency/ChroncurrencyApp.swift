1accd28089905dd953a1343b301fc49e2a2c5d6c
Aditya Singh <adisin650@gmail.com>
2025-07-19 13:29:56 -0700

    Initial Commit

---
+++ Chroncurrency/ChroncurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChroncurrencyApp.swift
+//  Chroncurrency
+//
+//  Created by Aditya Singh on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ChroncurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChroncurrencyTests/ChroncurrencyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChroncurrencyTests.swift
+//  ChroncurrencyTests
+//
+//  Created by Aditya Singh on 7/19/25.
+//
+
+import Testing
+@testable import Chroncurrency
+
+struct ChroncurrencyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
